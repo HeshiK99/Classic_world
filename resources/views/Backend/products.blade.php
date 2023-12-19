3adcd33d518365442e00001db5bd249547c7fe87
RusiruDananga <rusirudananga97@gmail.com>
2023-12-19 20:50:08 +0530

    added authentication

--- resources/views/Backend/products.blade.php
+++ resources/views/Backend/products.blade.php
@@ -38,6 +38,15 @@
                                     <input type="text" class="form-control" placeholder="Quantity" id="Quantity" name="productquantity">
                                 </div>
                             </div>
+                            <div class="custom-file-container" data-upload-id="myFirstImage">
+                                <label>Upload (Single File) <a href="javascript:void(0)" class="custom-file-container__image-clear" title="Clear Image">x</a></label>
+                                <label class="custom-file-container__custom-file" >
+                                    <input type="file" class="custom-file-container__custom-file__custom-file-input" accept="image/*">
+                                    <input type="hidden" name="MAX_FILE_SIZE" value="10485760" />
+                                    <span class="custom-file-container__custom-file__custom-file-control"></span>
+                                </label>
+                                <div class="custom-file-container__image-preview"></div>
+                            </div>
                             <input type="submit" name="time" class="btn btn-primary">
                         </form>
                     </div>
